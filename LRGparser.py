6855f90b9a01672c0a64164acc1a9c4e79973386
Rosie Coates-Brown <rosanna.coates-brown@postgrad.manchester.ac.uk>
2016-11-10 17:31:48 +0000

    sorted then broke csv, but we know why it's brokengit add LRGparser.py  added dict function, but this needs to be addressed

--- LRGparser.py
+++ LRGparser.py
@@ -31,7 +31,6 @@ def bed_file(root, gene):
     creates a dict with the exon ranges as keys, the exon_number as the value
     """
     exon_ranges={}
-
     ref_name = gene+"_37.bed"
     build37bed = open(ref_name, 'w')
 
@@ -59,6 +58,8 @@ def bed_file(root, gene):
                 start_int = int(start)
                 end = coord_sys.attrib['end']
                 end_int = int(end)
+                #exon_range = range(start_int,end_int)
+                #print (exon_range)
                 gen_st = str(start_int + offset_int)
                 gen_end = str(end_int + offset_int)
                 if gen_end > gen_st:
@@ -68,7 +69,7 @@ def bed_file(root, gene):
                     build37bed.write("\n")
                 elif gen_end <= gen_st:
                     print ("error: end coord is not greater than start")
-        exon_ranges[range(int(start), int(end))] = exon_number
+        exon_ranges[exon_number]=[start_int,end_int]
 
     #for keys, values in exon_ranges.items():
        #print(keys)
@@ -90,15 +91,27 @@ def get_diffs(exon_ranges):
                        for span in mapping:
                            for diff in span:
                                type = diff.attrib['type']
-                               lrg_start = diff.attrib['lrg_start']
-                               lrg_end =  diff.attrib['lrg_end']
-                               other_start = diff.attrib['other_start']
-                               other_end = diff.attrib['other_end']
+                               lrg_start = int(diff.attrib['lrg_start'])
+                               lrg_end =  int(diff.attrib['lrg_end'])
+                               other_start = int(diff.attrib['other_start'])
+                               other_end = int(diff.attrib['other_end'])
                                LRG_seq = diff.attrib['lrg_sequence']
                                other_seq = diff.attrib['other_sequence']
-                               diff_list = [type, lrg_start, lrg_end, other_start, other_end, LRG_seq, other_seq]
-                               diff_file.write(",".join(diff_list))
-                               diff_file.write("\n")
+                               for key, value in exon_ranges.items():
+                                   print(key)
+                                   print("\t")
+                                   print(value[0])
+                                   print("\t")
+                                   print(value[1])
+                                   #if lrg_start >= value[0] and lrg_start <= value[1]:
+                                        #print (key)
+                                #csv file doesn't work as all variables are integers- remove int and it will be fine
+                               #diff_list = [type, lrg_start, lrg_end, other_start, other_end, LRG_seq, other_seq]
+                               #diff_file.write(",".join(diff_list))
+                               #diff_file.write("\n")
+
+
+
 root, gene = read_file()
 exon_ranges = bed_file(root, gene)
 get_diffs(exon_ranges)
